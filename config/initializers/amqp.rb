f3f6ba37e034b8d625e5576936b7450ebf1374f2
Daniel <reis.danieloliveira@gmail.com>
2012-05-01 19:56:38 +0100

    initializer updated
    no big deal

--- config/initializers/amqp.rb
+++ config/initializers/amqp.rb
@@ -1,14 +1,10 @@
 require "rubygems"
 require "amqp"
 
-connection_settings = {
-  :host     => "192.168.1.84",
-  :port     => 10000
-}
 
 Thread.new { EM.run do
   
-  AMQP.connect(:host => "localhost", :port => 10000, :user => "guest", 
+  AMQP.connect(:host => "192.168.1.84", :port => 10000, :user => "guest", 
               :pass => "guest", :vhost => "/") do |connection|
      ch  = AMQP::Channel.new(connection)
      $AMQP_CH = ch
