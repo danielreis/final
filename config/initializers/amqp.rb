87a5e6e2e1445869019ab15681ee4b12fc1f9bc0
Daniel <reis.danieloliveira@gmail.com>
2012-05-02 12:24:00 +0100

    Change AMQP configs for deployment.

--- config/initializers/amqp.rb
+++ config/initializers/amqp.rb
@@ -4,7 +4,7 @@ require "amqp"
 
 Thread.new { EM.run do
   
-  AMQP.connect(:host => "192.168.1.84", :port => 10000, :user => "guest", 
+  AMQP.connect(:host => "127.0.0.1", :port => 10001, :user => "guest", 
               :pass => "guest", :vhost => "/") do |connection|
      ch  = AMQP::Channel.new(connection)
      $AMQP_CH = ch
